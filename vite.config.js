e70ee6c5190bd4daa634b4f196bf693362bd2df1
erzhan <erjik1902@gmail.com>
2025-05-06 19:23:56 +0600

    some path updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@app': path.resolve(__dirname, 'src/app'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@i': path.resolve(__dirname, 'icons'),
+		},
+	},
+});
